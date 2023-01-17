0c79f10dbd2c5cf5c832ec29cc2027b49d28057c
marcotommoro <marcomoroni99@gmail.com>
2023-01-17 16:17:37 +0100

    fix: minor fix

--- observer/src/index.ts
+++ observer/src/index.ts
@@ -5,33 +5,45 @@ let kafka: Kafka;
 let producer: Producer;
 
 const init = () => {
-  kafka = new Kafka({
-    clientId: 'observer',
-    brokers: ['kafka:9092'],
-  });
+  try {
+    kafka = new Kafka({
+      clientId: 'observer',
+      brokers: ['kafka:9092'],
+    });
 
-  producer = kafka.producer();
-  producer.connect();
+    producer = kafka.producer();
+    producer.connect();
+  } catch (error) {
+    console.log('Error connecting to Kafka');
+    setTimeout(() => {
+      init();
+    }, 2000);
+  }
 };
 
 const runListener = async () => {
-  const consumer = kafka.consumer({ groupId: 'observer-consumer' });
+  try {
+    const consumer = kafka.consumer({ groupId: 'observer-consumer' });
 
-  await consumer.connect();
-  await consumer.subscribe({
-    topic: 'messages-for-observer',
-    fromBeginning: true,
-  });
+    await consumer.connect();
+    await consumer.subscribe({
+      topic: 'messages-for-observer',
+      fromBeginning: true,
+    });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log('Received message');
-      console.log({
-        value: message.value.toString(),
-      });
-      sendMessageBack(message.value.toString());
-    },
-  });
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log('Received message');
+        console.log({
+          value: message.value.toString(),
+        });
+        sendMessageBack(message.value.toString());
+      },
+    });
+  } catch (error) {
+    console.log('Error connecting to Kafka');
+    console.log(error);
+  }
 };
 
 const sendMessageBack = async (message: string) => {
@@ -66,6 +78,13 @@ const sendMessageBack = async (message: string) => {
   });
 };
 
-init();
-runListener();
-initializeMongo();
+(async () => {
+  init();
+
+  initializeMongo();
+
+  while (!kafka) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+  runListener();
+})();
